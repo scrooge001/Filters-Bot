7fe00e35762a8b4267f8b1eb9257a85ad0bd46ad
Hansaka Anuhas <119654639+Hansaka-Anuhas@users.noreply.github.com>
2022-12-02 13:12:35 +0530

    Filters Bot

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001701705208'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SL_Auto_Filter_Bot_Support')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'SL_Filters_Bot_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
