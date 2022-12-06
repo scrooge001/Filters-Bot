3a4bf92ca1edbd12923bbbcbe7156f9d0f824c4a
HansakaAnuhas <119840634+HansakaAnuhas@users.noreply.github.com>
2022-12-06 18:32:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hansaka-Anuhas/Filters-Bot.git /Filters-Bot
+  git clone https://github.com/HansakaAnuhas/Filters-Bot.git /Filters-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Filters-Bot
